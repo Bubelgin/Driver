a60f68fc9f5b7954a74782294983fa8b96396718
Gintaras Bubelevicius <gintarasctx@gmail.com>
2018-05-31 10:32:06 +0100

    Script files

---
+++ Main-Stats-Upload-QA.ps1
@@ -0,0 +1,10 @@
+$AzureSQLServerName = "sql-neu-gps-upload-uk-qa"
+$Cred = Get-AutomationPSCredential -Name "CR-NEU-GPS-MAINT-UK-QA"
+$AzureSQLServerName = $AzureSQLServerName + ".database.windows.net"
+
+Write-Output "---------------------------------------------------------------"
+$AzureSQLDatabaseName = "db-neu-gps-upload-uk-qa"
+Write-Output "Index and Stats Maintenance for DB " $AzureSQLDatabaseName
+Write-Output "---------------------------------------------------------------"
+$SQLOutput = $(Invoke-Sqlcmd -ServerInstance $AzureSQLServerName -Username $Cred.UserName -Password $Cred.GetNetworkCredential().Password -Database $AzureSQLDatabaseName -Query "exec [dbo].[AzureSQLMaintenance] @Operation='statistics' ,@LogToTable=1" -QueryTimeout 65535 -ConnectionTimeout 60 -Verbose) 4>&1 
+Write-Output $SQLOutput
